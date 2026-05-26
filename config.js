9d9bd78215ae94dd734884c55660429cd0735c41
itnurse-swdcph <itnurse.swdcph@gmail.com>
2026-05-27 00:04:27 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.APP_CONFIG = {
-  apiBaseUrl:
-    "https://script.google.com/macros/s/AKfycbxypz_beCrlzGlQO4fE3HRn6lL9EnqOIDwyUDNYsxgyKZtoKjKLqh1YOcdYiRjEJowbgA/exec",
-  sheetId: "1ThrLfUFxATfqfUnAbhyZ8Ilqb5Hg2g_zhenkiRIEppE",
-  appName: "ระบบบันทึกทบทวน 12 กิจกรรม",
-  hospitalName: "โรงพยาบาลสมเด็จพระยุพราชสว่างแดนดิน",
-  missionName: "ภารกิจด้านการพยาบาล",
-};
