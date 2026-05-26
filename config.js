122cb560657e9a1eb93a2ce9729489658312e165
itnurse-swdcph <itnurse.swdcph@gmail.com>
2026-05-27 00:17:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   apiBaseUrl:
-    https://script.google.com/macros/s/AKfycbxypz_beCrlzGlQO4fE3HRn6lL9EnqOIDwyUDNYsxgyKZtoKjKLqh1YOcdYiRjEJowbgA/exec",
+    "https://script.google.com/macros/s/AKfycbxypz_beCrlzGlQO4fE3HRn6lL9EnqOIDwyUDNYsxgyKZtoKjKLqh1YOcdYiRjEJowbgA/exec",
   sheetId: "1ThrLfUFxATfqfUnAbhyZ8Ilqb5Hg2g_zhenkiRIEppE",
   appName: "ระบบทบทวน 12 กิจกรรมการพยาบาล",
   hospitalName: "โรงพยาบาลสมเด็จพระยุพราชสว่างแดนดิน",
