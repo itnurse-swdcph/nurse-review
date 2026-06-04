c619db34b7ffb0eed83b3cf33fda650d6fe70f7b
itnurse-swdcph <itnurse.swdcph@gmail.com>
2026-06-04 08:38:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ window.APP_CONFIG = {
   apiBaseUrl:
     "https://script.google.com/macros/s/AKfycbxypz_beCrlzGlQO4fE3HRn6lL9EnqOIDwyUDNYsxgyKZtoKjKLqh1YOcdYiRjEJowbgA/exec",
   sheetId: "1ThrLfUFxATfqfUnAbhyZ8Ilqb5Hg2g_zhenkiRIEppE",
-  appName: "ระบบทบทวน 12 กิจกรรมการพยาบาล",
+  appName: "ระบบบันทึกการทบทวน 12 กิจกรรม",
   hospitalName: "โรงพยาบาลสมเด็จพระยุพราชสว่างแดนดิน",
   missionName: "ภารกิจด้านการพยาบาล",
-  footerCredit: "© 2026 IM Nurse System All rights reserved. | Developed By Natnarinthorn",
+  footerCredit: "© 2026 Developed By Natnarinthorn",
   requestTimeoutMs: 45000,
   postRequestTimeoutMs: 120000,
   attachments: {
